e6be3dcd705e29d5231c545c0fee02101c88ce67
KARIMAUSIAS <karimausiasmarch@gmail.com>
2022-12-09 18:43:57 +0100

    first commit

---
+++ src/main/java/com/casamarruecos/CasaMarruecos/CasaMarruecosApplication.java
@@ -0,0 +1,13 @@
+package com.casamarruecos.CasaMarruecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaMarruecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaMarruecosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casamarruecos/CasaMarruecos/CasaMarruecosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casamarruecos.CasaMarruecos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaMarruecosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
